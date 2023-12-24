e22c10ef4631d0be40b0e61fb8ef79b239ce947c
Samson <samsondemessie@gmail.com>
2023-12-24 23:41:51 +0300

    note pad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Form from './components/Form';
+import { useState } from 'react';
 function App() {
+  const[note,setNotes]=useState([])
+  const [showForm, setShowForm]=useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      { showForm && <Form />}
+
+      <button className='show-btn' onClick={()=> setShowForm((prev)=>!prev)}>
+        {showForm? 'hide' : 'show'}
+      </button>
     </div>
   );
 }
